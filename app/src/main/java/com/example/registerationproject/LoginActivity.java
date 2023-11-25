c6aae312977c81b56d128c52db53ce2778f769e3
Hoyoung Park <qkrghdud369@gmail.com>
2023-11-26 01:19:58 +0900

    파이어베이스 연동 및 로그인 회원가입 화면 구성

--- app/src/main/java/com/example/registerationproject/LoginActivity.java
+++ app/src/main/java/com/example/registerationproject/LoginActivity.java
@@ -1,27 +1,97 @@
 package com.example.registerationproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView registerButton;
+    private Button loginButton;
+    private EditText idText;
+    private EditText passwordText;
+    private CheckBox login_chk;
+    private boolean saveLoginData;
+    private SharedPreferences appData;
+    private String id;
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView registerButton = findViewById(R.id.registerButton);
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        appData = getSharedPreferences("appData", MODE_PRIVATE);
+        load();
 
+        FirebaseApp.initializeApp(this);
+
+        registerButton =  findViewById(R.id.registerButton);
+        loginButton =  findViewById(R.id.loginButton);
+        idText =  findViewById(R.id.idText);
+        passwordText = ( findViewById(R.id.passwordText));
+        mAuth = FirebaseAuth.getInstance();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
-                LoginActivity.this.startActivity(registerIntent);
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
             }
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = idText.getText().toString().trim();
+                String pwd = passwordText.getText().toString().trim();
+                mAuth.signInWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {//성공했을때
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {//실패했을때
+                                    Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                save();
+            }
+        });
+
+    }
+
+    private void save() {
+
+        SharedPreferences.Editor editor = appData.edit();
+
+        editor.putString("ID", idText.getText().toString().trim());
+
+        // apply, commit 을 안하면 변경된 내용이 저장되지 않음
+        editor.apply();
+    }
+
+    private void load() {
+        saveLoginData = appData.getBoolean("SAVE_LOGIN_DATA", false);
+        id = appData.getString("ID", "");
     }
 }
--- app/src/main/java/com/example/registerationproject/RegisterActivity.java
+++ app/src/main/java/com/example/registerationproject/RegisterActivity.java
@@ -1,23 +1,97 @@
 package com.example.registerationproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
-    private ArrayAdapter adapter;
-    private Spinner spinner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity  {
+
+    private EditText et_register_id, et_register_pw, et_register_major, et_register_name;
+    private Button btn_register_button;
+
+    //파이어베이스 초기화
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+
+    final String FIRESTORE_TAG = "[FIRESTORE_TAG]";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {   // 액티비티 시작시 처음으로 시작되는 생명주기
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        spinner = findViewById(R.id.majorSpinner);
-        adapter = ArrayAdapter.createFromResource(this, R.array.major, android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        // xml파일에서 선언한 아이디 연결
+        et_register_id = findViewById(R.id.et_register_id);
+        et_register_pw = findViewById(R.id.et_register_pw);
+        et_register_name = findViewById(R.id.et_register_name);
+        et_register_major = findViewById(R.id.et_register_major);
+
+        btn_register_button = findViewById(R.id.btn_register_button);
+
+        btn_register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = et_register_id.getText().toString().trim();
+                final String password = et_register_pw.getText().toString().trim();
+                final String name = et_register_name.getText().toString().trim();
+                final String major = et_register_major.getText().toString().trim();
+
+                if ((email != null) && !email.isEmpty() && (password != null) && !password.isEmpty() && (name != null) && !name.isEmpty() && (major != null) && !major.isEmpty()) {
+                    mAuth.createUserWithEmailAndPassword(et_register_id.getText().toString(), et_register_pw.getText().toString())
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        Map<String, Object> userMap = new HashMap<>();
+                                        userMap.put(FirebaseID.documentId, user.getUid());
+                                        userMap.put(FirebaseID.email, email);
+                                        userMap.put(FirebaseID.password, password);
+                                        userMap.put(FirebaseID.name, name);
+                                        userMap.put(FirebaseID.major, major);
+                                        //현재 유저의 Uid를 이름으로 한 document 생성. 이게 없으면 사용자 컨텐츠의 이륾과 사용자id이름이 달라 사용하기 힘듬
+                                        mStore.collection(FirebaseID.user).document(user.getUid()).set(userMap, SetOptions.merge());
+
+                                        //회원가입 성공시 로그인 액티비티로 화면 전환
+                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(RegisterActivity.this, "회원가입 실패",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+
     }
 }
+
+
