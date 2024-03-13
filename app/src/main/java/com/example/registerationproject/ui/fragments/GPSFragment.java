4de2a861965e012ae97237b1b97888590c918499
Hoyoung Park <qkrghdud369@gmail.com>
2024-03-14 00:50:26 +0900

    네이버 지도 설정 및 지도 표출

--- app/src/main/java/com/example/registerationproject/ui/fragments/GPSFragment.java
+++ app/src/main/java/com/example/registerationproject/ui/fragments/GPSFragment.java
@@ -4,18 +4,25 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import androidx.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.registerationproject.R;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapSdk;
+import com.naver.maps.map.OnMapReadyCallback;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link GPSFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class GPSFragment extends Fragment {
+public class GPSFragment extends Fragment implements OnMapReadyCallback {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -47,6 +54,8 @@ public class GPSFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+    private MapView mapView;
+    private static NaverMap naverMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,7 +69,77 @@ public class GPSFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_g_p_s, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_g_p_s, container, false);
+        //네이버 지도
+        mapView = view.findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
+        this.naverMap = naverMap;
+        naverMap.setLayerGroupEnabled(naverMap.LAYER_GROUP_BUILDING, true);
+
+        CameraPosition cameraPosition = new CameraPosition(
+                new LatLng(33.38, 126.55),  // 위치 지정
+                12,                          // 줌 레벨
+                30,                          // 기울임 각도
+                45                           // 방향                          // 줌 레벨
+        );
+        naverMap.setCameraPosition(cameraPosition);
+    }
+    @Override
+    public void onStart()
+    {
+        String addr;
+
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroyView()
+    {
+        super.onDestroyView();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
